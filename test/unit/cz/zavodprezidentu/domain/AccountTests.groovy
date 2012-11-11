78e8b20d0e54bf0c6af57f8a86251da450dc130c
Michal Bernhard <michal@bernhard.cz>
2012-11-11 16:54:41 +0100

    Account basic save test

--- test/unit/cz/zavodprezidentu/domain/AccountTests.groovy
+++ test/unit/cz/zavodprezidentu/domain/AccountTests.groovy
@@ -3,7 +3,10 @@ package cz.zavodprezidentu.domain
 
 
 import grails.test.mixin.*
+import org.hamcrest.CoreMatchers
 import org.junit.*
+import static org.junit.Assert.*
+import static org.hamcrest.CoreMatchers.*
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -11,7 +14,16 @@ import org.junit.*
 @TestFor(Account)
 class AccountTests {
 
-    void testSomething() {
-        fail "Implement me"
+    @Test
+    public void "account with only balance should be saved"() {
+        BigDecimal balance = 1000.55
+
+        def account = new Account(balance: balance)
+        account.candidate = new Candidate(name: "Blue Kamikaze", account: account)
+        account = account.save(flush: true, failOnError: true)
+
+        def accountFromStorage = Account.findById(account.id)
+        assertThat("Account has not expected balance " + balance, accountFromStorage.balance, is(1000.55))
+
     }
 }
