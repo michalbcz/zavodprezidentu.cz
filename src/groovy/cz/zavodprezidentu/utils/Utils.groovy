e823bf42e925ff9041db0706a7f52a43e61fa7ed
Lukas Marek <lukas.marek@gmail.com>
2012-11-16 19:57:35 +0100

    Show at least one percent

--- src/groovy/cz/zavodprezidentu/utils/Utils.groovy
+++ src/groovy/cz/zavodprezidentu/utils/Utils.groovy
@@ -5,6 +5,11 @@ package cz.zavodprezidentu.utils
 class Utils {
     public static int getPercentage(BigDecimal amount, BigDecimal max) {
         def onePercent = max / (100)
-        return (amount / onePercent) as Integer;
+        def percentage = amount / onePercent
+        if (percentage < 1 && percentage > 0) {
+            percentage = 1
+        }
+
+        return percentage as Integer;
     }
 }
