3a39282ab1cfde448605451b3d92d413380b901c
Lukas Marek <lukas.marek@gmail.com>
2012-12-06 06:42:24 +0100

    Added tests

---
+++ test/unit/cz/zavodprezidentu/domain/ScraperTests.groovy
@@ -0,0 +1,16 @@
+package cz.zavodprezidentu.domain
+
+
+
+import grails.test.mixin.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Scraper)
+class ScraperTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
