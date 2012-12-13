a89acad898303e08226c5ea731a7a6ffac90f2fd
Michal Bernhard <michal@bernhard.cz>
2012-12-13 22:20:46 +0100

    cleaning scraper code

--- grails-app/services/zavod/prezidentu/ScraperService.groovy
+++ grails-app/services/zavod/prezidentu/ScraperService.groovy
@@ -24,17 +24,17 @@ class ScraperService {
 
         log.info("Scraping and saving candidates...")
 
-        new Candidate(
+        def fisherova = new Candidate(
                 name: "Taťána Fischerová",
                 image: "tatana_fischerova.jpg",
-                wikiUrl:  "http://cs.wikipedia.org/wiki/T%C3%A1%C5%88a_Fischerov%C3%A1",
-                account: new Account(number: "3", balance: new BigDecimal("0"))).save(failOnError: true)
+                wikiUrl:  "http://cs.wikipedia.org/wiki/T%C3%A1%C5%88a_Fischerov%C3%A1")
+        fisherova.save()
 
-        new Candidate(
+        def roithova = new Candidate(
                 name: "Zuzana Roithová",
                 image: "zuzana_roithova.jpg",
-                wikiUrl: "http://cs.wikipedia.org/wiki/Zuzana_Roithov%C3%A1",
-                account: new Account(number: "4", balance: new BigDecimal("0"))).save(failOnError: true)
+                wikiUrl: "http://cs.wikipedia.org/wiki/Zuzana_Roithov%C3%A1")
+        roithova.save()
 
 
         def zeman = new Candidate(
@@ -72,7 +72,7 @@ class ScraperService {
                 accountUrl: "https://www.fio.cz/scgi-bin/hermes/dz-transparent.cgi?pohyby_DAT_od=16.07.2012&ID_ucet=2100280379"
         )
         dienstbier.account = new FioAccountInfoScraper(url: dienstbier.accountUrl).account
-        dienstbier.save(failOnError: true)
+        dienstbier.save()
 
         def franz = new Candidate(
                 name: "Vladimír Franz",
@@ -81,7 +81,7 @@ class ScraperService {
                 accountUrl: "https://www.fio.cz/scgi-bin/hermes/dz-transparent.cgi?pohyby_DAT_od=02.10.2012&ID_ucet=2600311696"
         )
         franz.account = new FioAccountInfoScraper(url: franz.accountUrl).account
-        franz.save(failOnError: true)
+        franz.save()
 
         def fischer = new Candidate(
                 name: "Jan Fischer",
@@ -90,14 +90,13 @@ class ScraperService {
                 accountUrl: "http://www.rb.cz/firemni-finance/transparentni-ucty/?tr_acc=vypis&account_number=22200011"
         )
         fischer.account = new RaiffeisenAccountInfoScrapper(url: fischer.accountUrl).account
-        fischer.save(failOnError: true)
+        fischer.save()
 
         def bobosikova = new Candidate(
                 name: "Jana Bobošíková",
                 image: "jana_bobosikova.jpg",
                 accountUrl: null,
-                wikiUrl: "http://cs.wikipedia.org/wiki/Jana_Bobo%C5%A1%C3%ADkov%C3%A1",
-                account: new Account(number: "1", balance: new BigDecimal("0")))
+                wikiUrl: "http://cs.wikipedia.org/wiki/Jana_Bobo%C5%A1%C3%ADkov%C3%A1")
         bobosikova.save()
 
         Scraper lastRun = new Scraper(lastRun: new Date())
